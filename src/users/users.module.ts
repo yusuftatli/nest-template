84b6ed9e783bc85c972a43a812d0ff4b55db959d
Yusuf TATLI <yusuf@Yusufs-MacBook-Air.local>
2022-11-11 12:18:34 +0300

    add user table

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([User]),],
   providers: [UsersService],
   exports: [UsersService],
 })
